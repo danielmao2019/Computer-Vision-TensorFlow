c2ae0b06331e5e5006bf08cfbdbd96a02c11651d
danielmao2019 <danielmao2019@gmail.com>
2023-01-03 18:39:29 -0500

    [ResNet] ResNet testing with raw tensorflow built-in MNIST dataset

--- models/cnn/ResNet/test_ResNet.py
+++ models/cnn/ResNet/test_ResNet.py
@@ -1,21 +1,18 @@
 from ResNet import ResNet
 import tensorflow as tf
-import data
 
 
 if __name__ == "__main__":
-    image_h = image_w = 224
-    dataset = data.datasets.MNIST.MNISTDataset(purpose='training').tensorflow()
-    preprocessor = data.preprocess.Preprocessor(transforms=[
-        data.preprocess.image.ToTensor(),
-        data.preprocess.image.Resize(size=(image_h, image_w)),
-    ])
-    dataset = dataset.map(preprocessor)
-    dataset = dataset.batch(1)
+    image_h = image_w = 64
     model = ResNet(num_classes=10, version=18)
     model = model.build(input_shape=(image_h, image_w, 1))
     model.compile(
         loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        optimizer=tf.optimizers.SGD(),
     )
-    model.fit(dataset)
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    x_train = tf.expand_dims(x_train, axis=3)
+    x_test = tf.expand_dims(x_test, axis=3)
+    x_train = tf.image.resize(x_train, size=(image_h, image_w))
+    x_test = tf.image.resize(x_test, size=(image_h, image_w))
+    model.fit(x_train, y_train, epochs=1)
