8d9cd2c2f510bfaa8ee849f046489a0aadaff434
danielmao2019 <danielmao2019@gmail.com>
2023-01-04 21:41:46 -0500

    removed temp.py

--- temp.py
+++
@@ -1,7 +0,0 @@
-from models.cnn.SENet import SENet
-from models.cnn.MobileNets.MobileNetV3 import MobileNetV3
-
-if __name__ == "__main__":
-    model = MobileNetV3(model_type='small', output_dim=10)
-    model = model.build()
-    model.summary()
