b8777f891b98c895cb550bab9808468d77a2e6aa
danielmao2019 <danielmao2019@gmail.com>
2023-01-08 01:30:23 -0500

    [Training] Refactored testing code to use pytest
    * separated fast testing and slow testing of GitHub actions

--- training/test_train_model.py
+++ training/test_train_model.py
@@ -1,19 +1,23 @@
-from train_model import train_model
+import pytest
+from training import train_model
 import tensorflow as tf
 import data
 import models
 
 
-if __name__ == "__main__":
-    image_h = image_w = 128
+@pytest.mark.slow
+@pytest.mark.parametrize("image_size", [
+    (128, 128),
+])
+def test_train_model(image_size):
     dataset = data.datasets.MNISTDataset(purpose='training').take(16)
     preprocessor = data.preprocess.Preprocessor(transforms=[
-        data.preprocess.image.Resize(size=(image_h, image_w)),
+        data.preprocess.image.Resize(size=image_size),
     ])
     dataloader = data.dataloaders.Dataloader(dataset=dataset, shuffle=True, preprocessor=preprocessor, batch_size=4)
     print(f"{len(dataloader)=}")
     model = models.cnn.AlexNet(num_classes=10)
-    model = model.build(input_shape=(image_h, image_w, 1))
+    model = model.build(input_shape=image_size+(1,))
     loss = tf.keras.losses.SparseCategoricalCrossentropy()
     optimizer = tf.optimizers.SGD(learning_rate=1.0e-03)
     train_model(specs={
@@ -23,7 +27,7 @@ if __name__ == "__main__":
         'loss': loss,
         'optimizer': optimizer,
         'metric': lambda x, y: 0,
-        'epochs': 10,
+        'epochs': 1,
         'save_model': False,
         'load_model': None,
     })
