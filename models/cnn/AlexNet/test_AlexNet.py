801b747a3b3f2468eb0c2d755d20aab63181f260
tian xia <xt19901006@gmail.com>
2023-01-12 20:04:56 -0500

    [AlexNet] Refactored testing code to use pytest

--- models/cnn/AlexNet/test_AlexNet.py
+++ models/cnn/AlexNet/test_AlexNet.py
@@ -6,17 +6,31 @@ import logging
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.DEBUG)
 
 
-def test_forward_pass(model):
+@pytest.mark.dependency()
+@pytest.mark.parametrize("input_shape", [
+    (128, 128, 1), (128, 128, 3), (256, 256, 3),
+])
+
+def test_forward_pass(input_shape):
     logging.info(f"[RUNNING] Test case 'forward_pass' started.")
+    model = AlexNet(num_classes=10)
+    model = model.build(input_shape=input_shape)
     input = tf.zeros(shape=(1,)+model.layers[0].input_shape[0][1:])
+    logging.debug(f"{input.shape=}")
     output = model(input)
     assert len(output.shape) == 2, f"{output.shape=}"
     assert output.shape[0] == input.shape[0], f"{output.shape=}"
     logging.info("[PASSED] Test case 'forward_pass' passed.")
 
-
-def test_overfit(model):
+@pytest.mark.dependency(depends=['test_forward_pass'])
+@pytest.mark.slow
+@pytest.mark.parametrize("input_shape", [
+    (128, 128, 3),
+])
+def test_overfit(input_shape):
     logging.info(f"[RUNNING] Test case 'overfit' started.")
+    model = AlexNet(num_classes=10)
+    model = model.build(input_shape=input_shape)
     model.compile(
         loss=tf.keras.losses.SparseCategoricalCrossentropy(),
         optimizer=tf.optimizers.SGD(learning_rate=1.0e-03, momentum=0.9),
@@ -30,148 +44,8 @@ def test_overfit(model):
 
 
 if __name__ == "__main__":
-    # num_classes = 10
-    #case 1 
     model = AlexNet(num_classes=10)
     model = model.build(input_shape=(128, 128, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 2 
-    model = AlexNet(num_classes=10)
-    model = model.build(input_shape=(225, 225, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 3
-    model = AlexNet(num_classes=10)
-    model = model.build(input_shape=(128, 128, 1))
-    test_forward_pass(model)
-    test_overfit(model)
-    #num_Classes = 9
-    #case 1 
-    model = AlexNet(num_classes=9)
-    model = model.build(input_shape=(128, 128, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 2 
-    model = AlexNet(num_classes=9)
-    model = model.build(input_shape=(225, 225, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 3
-    model = AlexNet(num_classes=9)
-    model = model.build(input_shape=(128, 128, 1))
-    test_forward_pass(model)
-    test_overfit(model)
-    #num_Classes = 8
-    #case 1 
-    model = AlexNet(num_classes=8)
-    model = model.build(input_shape=(128, 128, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 2 
-    model = AlexNet(num_classes=8)
-    model = model.build(input_shape=(225, 225, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 3
-    model = AlexNet(num_classes=8)
-    model = model.build(input_shape=(128, 128, 1))
-    test_forward_pass(model)
-    test_overfit(model)
-    #num_Classes = 7
-    #case 1 
-    model = AlexNet(num_classes=7)
-    model = model.build(input_shape=(128, 128, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 2 
-    model = AlexNet(num_classes=7)
-    model = model.build(input_shape=(225, 225, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 3
-    model = AlexNet(num_classes=7)
-    model = model.build(input_shape=(128, 128, 1))
-    test_forward_pass(model)
-    test_overfit(model)
-    #num_Classes = 6
-    #case 1 
-    model = AlexNet(num_classes=6)
-    model = model.build(input_shape=(128, 128, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 2 
-    model = AlexNet(num_classes=6)
-    model = model.build(input_shape=(225, 225, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 3
-    model = AlexNet(num_classes=6)
-    model = model.build(input_shape=(128, 128, 1))
-    test_forward_pass(model)
-    test_overfit(model)
-    #num_Classes = 5
-    #case 1 
-    model = AlexNet(num_classes=5)
-    model = model.build(input_shape=(128, 128, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 2 
-    model = AlexNet(num_classes=5)
-    model = model.build(input_shape=(225, 225, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 3
-    model = AlexNet(num_classes=5)
-    model = model.build(input_shape=(128, 128, 1))
-    test_forward_pass(model)
-    test_overfit(model)
-    #num_Classes = 4
-    #case 1 
-    model = AlexNet(num_classes=4)
-    model = model.build(input_shape=(128, 128, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 2 
-    model = AlexNet(num_classes=4)
-    model = model.build(input_shape=(225, 225, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 3
-    model = AlexNet(num_classes=4)
-    model = model.build(input_shape=(128, 128, 1))
-    test_forward_pass(model)
-    test_overfit(model)
-    #num_Classes = 3
-    #case 1 
-    model = AlexNet(num_classes=3)
-    model = model.build(input_shape=(128, 128, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 2 
-    model = AlexNet(num_classes=3)
-    model = model.build(input_shape=(225, 225, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 3
-    model = AlexNet(num_classes=3)
-    model = model.build(input_shape=(128, 128, 1))
-    test_forward_pass(model)
-    test_overfit(model)
-    #num_Classes = 2
-    #case 1 
-    model = AlexNet(num_classes=2)
-    model = model.build(input_shape=(128, 128, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 2 
-    model = AlexNet(num_classes=2)
-    model = model.build(input_shape=(225, 225, 3))
-    test_forward_pass(model)
-    test_overfit(model)
-    #case 3
-    model = AlexNet(num_classes=2)
-    model = model.build(input_shape=(128, 128, 1))
     test_forward_pass(model)
     test_overfit(model)
     
