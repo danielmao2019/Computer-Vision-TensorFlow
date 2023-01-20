936859e17095489edc1a3e745a0a2b69778ce7c6
tian xia <xt19901006@gmail.com>
2023-01-20 01:07:20 -0500

    [ResNet]

--- models/cnn/ResNet/test_ResNet.py
+++ models/cnn/ResNet/test_ResNet.py
@@ -33,18 +33,15 @@ def test_overfit(input_shape):
         loss=loss,
         optimizer=tf.optimizers.SGD(),
     )
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    x_train = tf.expand_dims(x_train, axis=3)
-    x_test = tf.expand_dims(x_test, axis=3)
-    x_train = tf.image.resize(x_train, size=(image_h, image_w))
-    x_test = tf.image.resize(x_test, size=(image_h, image_w))
+    x_train = tf.random.uniform(shape=(1,)+model.layers[0].input_shape[0][1:])
+    y_train = tf.zeros(shape=(1,), dtype=tf.int64)
     logging.debug(f"{x_train.shape=}")
     logging.debug(f"{y_train.shape=}")
     model.trainable = True
-    model.fit(x_train, y_train, epochs=1)
+    model.fit(x_train, y_train, epochs=1000)
     model.trainable = False
     error = loss(y_true=y_train, y_pred=model(x_train))
-    #assert error <= 2.0e-5, f"{error=}"
+    assert error <= 1.0e-5, f"{error=}"
 
 
 if __name__ == "__main__":
