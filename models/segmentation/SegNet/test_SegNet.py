ceccfd365d87206cc01c93d30106c7fceb6d79af
tian xia <xt19901006@gmail.com>
2023-01-23 18:05:29 -0500

    [ResNet]

--- models/segmentation/SegNet/test_SegNet.py
+++ models/segmentation/SegNet/test_SegNet.py
@@ -33,8 +33,18 @@ def test_overfit(input_shape):
         loss=tf.keras.losses.SparseCategoricalCrossentropy(),
         optimizer=tf.optimizers.SGD(learning_rate=1.0e-01, momentum=0.9),
     )
-    x_train = tf.random.uniform(shape=(1,)+model.layers[0].input_shape[0][1:])
-    y_train = tf.zeros(shape=(1,)+model.layers[0].input_shape[0][1:3], dtype=tf.int64)
+    #x_train = tf.random.uniform(shape=(1,)+model.layers[0].input_shape[0][1:])
+    #y_train = tf.zeros(shape=(1,)+model.layers[0].input_shape[0][1:3], dtype=tf.int64)
+    x_train = tf.concat([
+    tf.random.uniform(shape=(1,)+model.layers[0].input_shape[0][1:]),
+    tf.ones(shape=(1,)+model.layers[0].input_shape[0][1:], dtype=tf.float32),
+    tf.random.normal(shape=(1,)+model.layers[0].input_shape[0][1:]),
+], axis=0)
+    y_train = tf.concat([
+    tf.zeros(shape=(1,)+model.layers[0].input_shape[0][1:3], dtype=tf.int64),
+    tf.zeros(shape=(1,)+model.layers[0].input_shape[0][1:3], dtype=tf.int64),
+    tf.zeros(shape=(1,)+model.layers[0].input_shape[0][1:3], dtype=tf.int64),
+], axis=0)
     logging.debug(f"{x_train.shape=}")
     logging.debug(f"{y_train.shape=}")
     model.trainable = True
